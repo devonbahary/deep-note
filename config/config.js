d4ac92d3669a4a2409badc90d9f65347bba68d24
devonbahary <devonbahary@gmail.com>
2018-10-11 23:11:41 -0700

    Adds mLab MONGODB_URI

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ if (process.env.NODE_ENV === 'test') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/sample-app-test';
 } else if (process.env.NODE_ENV === 'dev') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/sample-app';
+} else {
+  process.env.MONGODB_URI = 'mongodb://heroku_bszzpjgw:tjvtrpp3pe1e7rprjkf2no0e98@ds231133.mlab.com:31133/heroku_bszzpjgw';
 }
-
-// ELSE: add your MONGODB_URI here
