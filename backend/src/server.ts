422551f5e8cbec5825871671f0d5881816f0f2d7
Devon Bahary <devonbahary@gmail.com>
2024-01-27 12:26:36 -0800

    feat: prettier configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+const app = express()
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
